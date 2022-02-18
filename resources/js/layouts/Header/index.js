1985dd93e22f60b16156757ea9bd4958a536345b
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-02-18 15:46:08 +0800

    Header is now fully responsive

--- resources/js/layouts/Header/index.js
+++ resources/js/layouts/Header/index.js
@@ -8,25 +8,27 @@ const Header = () => {
   const { showNav, setShowNav } = useHeader();
 
   return (
-    <div className="h-[60px] px-4 flex items-center justify-between shadow-sm">
-      <div className="w-fit flex items-center">
-        <img
-          className="w-[40px] h-[40px] rounded-full"
-          src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          alt="Profile picture"
+    <div className="px-4 shadow-sm">
+      <div className="wrapper h-[60px] flex items-center justify-between">
+        <div className="w-fit flex items-center">
+          <img
+            className="w-[40px] h-[40px] rounded-full"
+            src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            alt="Profile picture"
+          />
+          <span className="text-[15px] ml-2 font-medium">{user.firstName} {user.lastName}</span>
+        </div>
+
+        <HamburgerMenuIcon
+          onToggle={() => setShowNav(!showNav)}
         />
-        <span className="text-[15px] ml-2 font-medium">{user.firstName} {user.lastName}</span>
+
+        <Navigation
+          className={showNav ? 'flex' : 'hidden'}
+          onSwitchToCollections={() => console.log('Collections')}
+          onLogout={() => console.log('Logout')}
+        />
       </div>
-
-      <HamburgerMenuIcon
-        onToggle={() => setShowNav(!showNav)}
-      />
-
-      <Navigation
-        className={showNav ? 'flex' : 'hidden'}
-        onSwitchToCollections={null}
-        onLogout={null}
-      />
     </div>
   );
 }
--- resources/js/layouts/Navigation/index.js
+++ resources/js/layouts/Navigation/index.js
@@ -1,21 +1,21 @@
 import Button from '@common/Button';
 import Toggler from '@common/Toggler';
 
-const Navigation = ({ className, onSwtichToCollections, onLogout }) => {
+const Navigation = ({ className, onSwitchToCollections, onLogout }) => {
   return (
-    <div className={`${className} fixed left-0 top-[60px] self-start flex-col justify-center items-center h-full w-full`}>
+    <div className={`${className} lg:flex fixed lg:static left-0 top-[60px] self-start flex-col lg:flex-row justify-center items-center h-full w-full lg:w-fit`}>
       <Toggler
-        className="absolute top-[20px] right-[20px]"
+        className="absolute lg:static top-[20px] right-[20px]"
         onEnable={() => console.log('toggler ON')}
         onDisable={() => console.log('toggler OFF')}
       />
       <Button
-        className="w-[150px]"
-        onClick={() => onSwtichToCollections()}
+        className="w-[150px] lg:h-[40px] lg:ml-8 lg:text-[15px]"
+        onClick={() => onSwitchToCollections()}
         label="Collections"
       />
       <button
-        className="block mt-4 h-[50px] w-fit px-4"
+        className="block mt-4 lg:mt-0 lg:ml-4 h-[50px] w-fit px-4 lg:text-[15px]"
         onClick={() => onLogout()}
       >
         Logout
