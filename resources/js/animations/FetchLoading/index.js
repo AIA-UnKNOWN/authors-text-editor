8dbeca9f14148fe658e52566c0eb1c69f05c4816
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-02-20 17:51:46 +0800

    Added loading animation when getting Collections

--- resources/js/animations/FetchLoading/index.js
+++ resources/js/animations/FetchLoading/index.js
@@ -1,7 +1,9 @@
+import './style.css';
+
 const FetchLoading = () => {
   return (
-    <div>
-
+    <div className="flex flex-col flex-1 justify-center items-center mb-4">
+      <div className="fetch-loading border-[7px] border-pink border-t-8 border-t-white h-[40px] w-[40px] rounded-full shadow-md"></div>
     </div>
   );
 }
--- resources/js/sub-pages/Collections/index.js
+++ resources/js/sub-pages/Collections/index.js
@@ -1,27 +1,32 @@
 import { useSelector } from 'react-redux';
 import useCollections from './hook';
 import Collection from './Collection';
+import FetchLoading from '@animations/FetchLoading';
 
 const Collections = () => {
   const collections = useSelector(state => state.collections.list);
   useCollections();
 
   return (
-    <div className="h-full">
-      <div className="wrapper w-full lg:w-[800px] bg-light-gray-1 h-full">
-        <div className="h-full px-4 w-full md:w-[540px] mx-auto">
+    <div className="flex flex-col flex-1">
+      <div className="wrapper w-full lg:w-[800px] bg-light-gray-1 flex flex-col flex-1">
+        <div className="flex flex-col flex-1 px-4 w-full md:w-[540px] mx-auto">
           <p className="text-[30px] font-medium text-center py-4 border-b border-dark-gray">
             Collections
           </p>
           
-          <div className="pt-4">
-            {collections.length > 0 && collections.map(collection => (
-              <Collection
-                key={collection.id}
-                data={collection}
-              />
-            ))}
-          </div>
+          {collections.length > 0 ? (
+            <div className="pt-4 flex flex-col flex-1 overflow-auto">
+              {collections.map(collection => (
+                <Collection
+                  key={collection.id}
+                  data={collection}
+                />
+              ))}
+            </div>
+          ) : (
+            <FetchLoading />
+          )}
         </div>
       </div>
     </div>
