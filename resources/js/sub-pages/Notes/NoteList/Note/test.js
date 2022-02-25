f665ec8f905c3c06c0cf63131d73e55c071d10c7
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-02-26 02:35:55 +0800

    Add snapshot test for Note component

---
+++ resources/js/sub-pages/Notes/NoteList/Note/test.js
@@ -0,0 +1,22 @@
+import 'regenerator-runtime';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Note from './index';
+
+test('Note renders correctly', () => {
+  const initialState = {
+    notes: {
+      collectionId: 1
+    }
+  };
+  const mockStore = configureStore();
+
+  const component = renderer.create(
+    <Provider store={mockStore(initialState)}>
+      <Note />
+    </Provider>
+  );
+  const NoteComponent = component.toJSON();
+  expect(NoteComponent).toMatchSnapshot();
+});
